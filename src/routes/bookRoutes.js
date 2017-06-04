0728147463b255ba1fd8ec36ed4799cf9a8db03a
mohit uprim <mohit uprim>
2017-06-05 00:48:05 +0530

    Structure and 3rd Party APIs

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,54 +4,16 @@ var objectId = require('mongodb').ObjectID;
 var bookRouter = express.Router();
 
 var router = function(nav){
-
-bookRouter.use(function(req, res, next){
-    if (!req.user) {
-                res.redirect('/');
-    }
-    next();
-});
+    var bookService = require('../services/goodreadsService')();
+var bookController = require('../controllers/bookController')(bookService, nav);
+bookRouter.use(bookController.middleware);
 bookRouter.route('/')
-    .get(function(req, res) {
-
-         var url =
-                'mongodb://localhost:27017/libraryApp';
-
-        mongodb.connect(url, function (err, db) {
-            var collection =  db.collection('books');
-            collection.find({}).toArray(
-                function(err, results){
-                    res.render('bookListView',{
-                    title:'Hello From Books',
-                    nav: nav, 
-                    books: results
-                });
-            });
-        });
-       
-});
+    .get(bookController.getIndex);
 
 bookRouter.route('/:id')
-    .get(function(req, res) {
-        var id = new objectId(req.params.id);
-         var url =
-                'mongodb://localhost:27017/libraryApp';
+    .get(bookController.getById);
 
-        mongodb.connect(url, function (err, db) {
-            var collection =  db.collection('books');
-            collection.findOne({_id: id},
-                function(err, results){
-                    res.render('bookView',{
-                    title:'Hello From Books',
-                    nav: nav, 
-                    book: results
-                });
-            });
-        });
-    });
-
-    return bookRouter;
+return bookRouter;
 }
-
 
 module.exports=router;
